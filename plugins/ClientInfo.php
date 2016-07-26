4e8cca266880c30418e4cd67d8b5fdbc857748bc
DrWhat <DrWhat@cryto.net>
2016-07-26 08:19:51 +0100

    Correct code style

--- plugins/ClientInfo.php
+++ plugins/ClientInfo.php
@@ -17,18 +17,18 @@ class ClientInfo extends Plugin
         }
         $this->server = $this->teamSpeak3Bot->node;
         try{
-            $Client = $this->server->clientGetByName($this->info['invokername']);
+            $client = $this->server->clientGetByName($this->info['invokername']);
             $name = $this->info['text'];
             $clientInfo = $this->server->clientInfoDb($this->server->clientFindDb($this->info['text']));
 
-            $Client->message("[COLOR=blue][B]{$name}: Database ID: {$clientInfo["client_database_id"]}");
-            $Client->message("[COLOR=blue][B]{$name}: Unique ID: {$clientInfo["client_unique_identifier"]}");
-            $Client->message("[COLOR=blue][B]{$name}: Joined: ".date("F j, Y, g:i a",$clientInfo["client_created"]));
-            $Client->message("[COLOR=blue][B]{$name}: Last connection: ". date("F j, Y, g:i a",$clientInfo["client_lastconnected"]));
-            $Client->message("[COLOR=blue][B]{$name}: Total connections: {$clientInfo["client_totalconnections"]}");
+            $client->message("[COLOR=blue][B]{$name}: Database ID: {$clientInfo["client_database_id"]}");
+            $client->message("[COLOR=blue][B]{$name}: Unique ID: {$clientInfo["client_unique_identifier"]}");
+            $client->message("[COLOR=blue][B]{$name}: Joined: ".date("F j, Y, g:i a",$clientInfo["client_created"]));
+            $client->message("[COLOR=blue][B]{$name}: Last connection: ". date("F j, Y, g:i a",$clientInfo["client_lastconnected"]));
+            $client->message("[COLOR=blue][B]{$name}: Total connections: {$clientInfo["client_totalconnections"]}");
             $clientInfo["client_description"] = ($clientInfo["client_description"])?$clientInfo["client_description"]:"N/A";
-            $Client->message("[COLOR=blue][B]{$name}: Client description: {$clientInfo["client_description"]}");
-            $Client->message("[COLOR=blue][B]{$name}: Last IP: {$clientInfo["client_lastip"]}");
+            $client->message("[COLOR=blue][B]{$name}: Client description: {$clientInfo["client_description"]}");
+            $client->message("[COLOR=blue][B]{$name}: Last IP: {$clientInfo["client_lastip"]}");
         }catch (Ts3Exception $e)
         {
             $message = $e->getMessage();
