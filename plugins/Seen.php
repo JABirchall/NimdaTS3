1a49adfc46bb7f91cece54dd5c891054b994132c
DrWhat <DrWhat@cryto.net>
2016-07-28 15:33:46 +0100

    plugins/Seen.php built

--- plugins/Seen.php
+++ plugins/Seen.php
@@ -13,10 +13,23 @@ class Seen extends Plugin
     {
         if (!isset($this->info['text'])) {
             $this->sendOutput($this->CONFIG['usage']);
-
             return;
         }
         $this->server = $this->teamSpeak3Bot->node;
+
+        try{
+            $client = $this->server->clientGetByName($this->info['invokername']);
+            $name = $this->info['text'];
+            $seen = $this->server->clientInfoDb($this->server->clientFindDb($this->info['text']));
+
+            $client->message("[COLOR=blue][B]User {$name} was last seen on ". date("F j, Y, g:i a",$seen["client_lastconnected"]));
+
+        }catch (Ts3Exception $e)
+        {
+            $message = $e->getMessage();
+            $admin = $this->server->clientGetByName($this->info['invokername']);
+            $admin->message("[color=red][b]ERROR : {$message}");
+        }
 
     }
 }
