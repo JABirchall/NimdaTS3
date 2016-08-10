e1f448fdc5253f88b8be5e9cdb5bb04831754e7e
Jake Birchall <drwhat@cryto.net>
2016-08-10 22:47:57 +0100

    use config

--- config/Teamspeak.php
+++ config/Teamspeak.php
@@ -8,7 +8,6 @@
 
 namespace Config;
 
-
 class Teamspeak
 {
 
--- start.php
+++ start.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\TeamSpeak3Bot;
+use Config\Teamspeak;
 
 include_once(__DIR__ . "/vendor/autoload.php");
 
-TeamSpeak3Bot::setOptions((new \Config\Teamspeak)->config);
+TeamSpeak3Bot::setOptions((new Teamspeak)->config);
 
 $bot = TeamSpeak3Bot::getInstance();
 $bot->run();
