d05174df4d07397dbed569eabf14d66a760572f2
Jake Birchall <drwhat@cryto.net>
2016-08-12 02:09:29 +0100

    nimda

--- config/database.php
+++ config/database.php
@@ -32,10 +32,10 @@ class Database
         |
         | Here are each of the database connections setup for your application.
         | Of course, examples of configuring each database platform that is
-        | supported by Laravel is shown below to make development simple.
+        | supported by Nimda is shown below to make development simple.
         |
         |
-        | All database work in Laravel is done through the PHP PDO facilities
+        | All database work in Nimda is done through the PHP PDO facilities
         | so make sure you have the driver for your particular database of
         | choice installed on your machine before you begin development.
         |
