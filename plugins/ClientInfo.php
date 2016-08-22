0e8245005ea8132ad5a9634449443fc3c858b7d0
Jake Birchall <drwhat@cryto.net>
2016-08-23 00:10:53 +0100

    Was going to format, No no monospacing on teamspeak so this will do.

--- plugins/ClientInfo.php
+++ plugins/ClientInfo.php
@@ -27,13 +27,13 @@ class ClientInfo extends Plugin implements PluginContract
             $name = $this->info['text'];
             $clientInfo = $this->server->clientInfoDb($this->server->clientFindDb($this->info['text']));
 
-            $this->sendOutput("[COLOR=blue][B]%s: Database ID: %s", $name, $clientInfo["client_database_id"]);
-            $this->sendOutput("[COLOR=blue][B]%s: Unique ID: %s", $name, $clientInfo["client_unique_identifier"]);
-            $this->sendOutput("[COLOR=blue][B]%s: Joined: %s", $name, Carbon::createFromTimestamp($clientInfo["client_created"])->toDateTimeString());
-            $this->sendOutput("[COLOR=blue][B]%s: Last connection: %s", $name, Carbon::createFromTimestamp($clientInfo["client_lastconnected"])->diffForHumans());
-            $this->sendOutput("[COLOR=blue][B]%s: Total connections: %s", $name, $clientInfo["client_totalconnections"]);
-            $this->sendOutput("[COLOR=blue][B]%s: Client description: %s", $name, ($clientInfo["client_description"]) ? $clientInfo["client_description"] : "N/A");
-            $this->sendOutput("[COLOR=blue][B]%s: Last IP: %s", $name, $clientInfo["client_lastip"]);
+            $this->sendOutput("[COLOR=blue][B]%s - Database ID: %s", $name, $clientInfo["client_database_id"]);
+            $this->sendOutput("[COLOR=blue][B]%s - Unique ID: %s", $name, $clientInfo["client_unique_identifier"]);
+            $this->sendOutput("[COLOR=blue][B]%s - First joined: %s", $name, Carbon::createFromTimestamp($clientInfo["client_created"])->toDateTimeString());
+            $this->sendOutput("[COLOR=blue][B]%s - Last connection: %s", $name, Carbon::createFromTimestamp($clientInfo["client_lastconnected"])->diffForHumans());
+            $this->sendOutput("[COLOR=blue][B]%s - Total connections: %s", $name, $clientInfo["client_totalconnections"]);
+            $this->sendOutput("[COLOR=blue][B]%s - Client description: %s", $name, ($clientInfo["client_description"]) ? $clientInfo["client_description"] : "N/A");
+            $this->sendOutput("[COLOR=blue][B]%s - Last IP: %s", $name, $clientInfo["client_lastip"]);
         } catch (Ts3Exception $e) {
             $this->sendOutput("[color=red][b]ERROR : %s", $e->getMessage());
         }
