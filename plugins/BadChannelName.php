96414e611f5dabae92bcc60cc17e5ccc21e6e51f
Jake Birchall <drwhat@cryto.net>
2016-08-23 01:12:36 +0100

    ChannelName checking and handling.

--- plugins/BadChannelName.php
+++ plugins/BadChannelName.php
@@ -10,6 +10,8 @@ namespace Plugin;
 
 
 use App\Plugin;
+use TeamSpeak3\TeamSpeak3;
+use TeamSpeak3\Ts3Exception;
 
 class BadChannelName extends Plugin implements PluginContract
 {
@@ -20,8 +22,26 @@ class BadChannelName extends Plugin implements PluginContract
     {
         $this->server = $this->teamSpeak3Bot->node;
 
-        // TODO: Implement isTriggered() method.
+        foreach($this->CONFIG['blacklist'] as $regex) {
+            if(!$this->info['channel_name']->contains($regex, true)) {
+                continue;
+            }
 
+            $this->handle();
+            return;
+        }
+    }
 
+    private function handle()
+    {
+        echo "match";
+        $channel = $this->server->channelGetById(@$this->info['ctid']?$this->info['ctid']:$this->info['cid']);
+        $violator = $this->server->clientGetById($this->info['invokerid']?$this->info['invokerid']:$this->info['clid']);
+        try {
+            $channel->delete(true);
+            $violator->kick(TeamSpeak3::KICK_SERVER, "Channel Name Violation");
+        } catch(Ts3Exception $e) {
+            echo $e->getMessage();
+        }
     }
 }
