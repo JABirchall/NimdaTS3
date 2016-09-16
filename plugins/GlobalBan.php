d5cd76017c903c56130fcfa4889db46d162526b4
Jake Birchall <drwhat@cryto.net>
2016-09-16 06:24:04 +0100

    Global ban bootstrap

---
+++ plugins/GlobalBan.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jake
+ * Date: 16/09/2016
+ * Time: 06:18
+ */
+
+namespace Plugin;
+
+
+use App\Plugin;
+
+class GlobalBan extends Plugin implements PluginContract
+{
+
+    public function isTriggered()
+    {
+        // TODO: Implement isTriggered() method.
+    }
+
+}
---
+++ plugins/GlobalBanList.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jake
+ * Date: 16/09/2016
+ * Time: 06:18
+ */
+
+namespace Plugin;
+
+
+use App\Plugin;
+
+class GlobalBanList extends Plugin implements AdvancedPluginContract
+{
+
+    public function isTriggered()
+    {
+        // TODO: Implement isTriggered() method.
+    }
+
+    public function install()
+    {
+        // TODO: Implement install() method.
+    }
+
+    public function update($version)
+    {
+        // TODO: Implement update() method.
+    }
+}
